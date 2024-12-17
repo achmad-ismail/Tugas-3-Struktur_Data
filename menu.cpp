fdfccc434149c1841e5bb7b4f6c45c111997bbc0
achmad-ismail <achmadismail.poliban@gmail.com>
2024-12-17 20:33:11 +0800

    u:penyesuaian isi halaman

---
+++ menu.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void dMenu(){
+system("cls");
+cout<<"Aplikasi Sorting Bubble"<<"\n";       
+cout<<"1. Masukkan data"<<"\n";            
+cout<<"2. Tampilan data"<<"\n";            
+cout<<"3. Sorting asc"<<"\n";           
+cout<<"4. Sorting dsc"<<"\n";            
+cout<<"5. Exit"<<"\n";           
+cout<<"Masukan angka :";        
+}
+
+void mPertama(string pesan){
+system("cls");
+if (pesan == "pertama") {
+  cout << "Masukkan jumlah data: ";
+  cin >> n;
+  for (int i=0; i<n; i++){
+    cout << "Masukkan data ke-" << (i+)
+  }
+}
+getch();
+}
+
+
+int main() {
+char pl;
+do
+{
+    dMenu();
+    pl=getch();
+  switch (pl)
+  {
+   case '1':
+    /* code */
+    mPertama("pertama");
+    break;
+   case '2':
+    mPertama("ke- dua");
+    /* code */ 
+    break;  
+   case '3':
+    mPertama("ke- tiga");
+    /* code */
+    break;  
+   case '4':
+    mPertama("ke- empat");
+    /* code */
+    break;  
+  case '5':
+    /* code */
+    break;
+  
+  default:
+    system("cls");
+    cout<<"Pilihan Tidak Tersedia";
+    getch();
+    break;
+  }
+
+
+} while (pl!='5');
+  return 0;
+}
